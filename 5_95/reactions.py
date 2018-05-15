089736ae34b551795becdef9cb4c1de8c99cb5c3
John Boyington <mjcb@ksu.edu>
2018-05-15 13:26:40 -0500

    put xsections into separate folder

--- 5_95/reactions.py
+++ 5_95/reactions.py
@@ -2,8 +2,8 @@ from roi import calc_roi
 
 reactions = {}
 
-reactions['Al_np'] = ['13-Al-27(n,p)', 1E4, 1E7]
-reactions['Al_na'] = ['13-Al-27(n,&alpha;)', 5E6, 1E7]
+reactions['Al_np'] = ['../xs/13-Al-27(n,p)', 1E4, 1E7]
+reactions['Al_na'] = ['../xs/13-Al-27(n,&alpha;)', 5E6, 1E7]
 
 for key, vals in reactions.items():
     reactions[key].append(calc_roi(*vals))
